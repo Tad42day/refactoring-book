9cd4b47d56ecaa01654d9779130636c2a93ca697
Thadeu Alonso <thadeu.alonso@gmail.com>
2018-05-01 22:41:35 -0300

    Instancias criadas na classe "Main" para testes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Teste");
+        Customer customer = new Customer("Thadeu");
+        Movie[] movies = new Movie[3];
+        Rental[] rentals = new Rental[3];
+        
+        movies[0] = new Movie("Rei leão", 2);
+        movies[1] = new Movie("Jumanji", 0);
+        movies[2] = new Movie("Vingadores", 1);
+        
+        for (int i = 0; i < movies.length; i++) {
+            rentals[i] = new Rental(movies[i], 2);
+            customer.addRental(rentals[i]);
+        }
+        
+        System.out.println(customer.statement());
     }
 }
